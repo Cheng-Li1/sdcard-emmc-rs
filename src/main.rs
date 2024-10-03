14c57982d8de00a93579852a57eb7cff4ed91fdf
Cheng <lichengchaoreng@gmail.com>
2024-10-03 20:37:02 +1000

    Code refactor(fearless refactor!)
    Signed-off-by: Cheng <lichengchaoreng@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -5,17 +5,14 @@ extern crate alloc;
 
 mod sddf_blk;
 
-use core::{future::{self, Future}, pin::{self, Pin}, task::{Context, Poll, RawWaker, RawWakerVTable, Waker}};
+use core::{future::Future, pin::Pin, task::{Context, Poll, RawWaker, RawWakerVTable, Waker}};
 
 use alloc::boxed::Box;
 use sddf_blk::{blk_dequeue_req_helper, blk_enqueue_resp_helper, blk_queue_empty_req_helper, blk_queue_full_resp_helper, blk_queue_init_helper, BlkOp, BlkStatus};
 use sdmmc_hal::meson_gx_mmc::MesonSdmmcRegisters;
 
-use sdmmc_protocol::sdmmc::{self, SdmmcHalError, SdmmcHardware, SdmmcProtocol};
+use sdmmc_protocol::sdmmc::{SdmmcHalError, SdmmcHardware, SdmmcProtocol};
 use sel4_microkit::{debug_print, debug_println, protection_domain, Channel, Handler, Infallible};
-
-const SDMMC_BASE_ADDR: *mut MesonSdmmcRegisters = 0xffe05000 as *mut MesonSdmmcRegisters;
-const DATA_ADDR: *mut u8 = 0x50000000 as *mut u8;
 
 const BLK_VIRTUALIZER: sel4_microkit::Channel = sel4_microkit::Channel::new(0);
 
@@ -80,18 +77,6 @@ impl<'a, T: SdmmcHardware> Handler for HandlerImpl<'a, T> {
     fn notified(&mut self, channel: Channel) -> Result<(), Self::Error> {
         match channel {
             BLK_VIRTUALIZER => {
-                // If the future is some, block itself
-                // Since we polling on 
-                if self.future.is_some() {
-                    debug_println!("SDMMC_DRIVER: The future should not exists here!!!");
-                    return Ok(())
-                }
-                unsafe {
-                    // If request queue is empty or resp queue is full, do not dequeue request
-                    if blk_queue_empty_req_helper() > 0 || blk_queue_full_resp_helper() > 0 {
-                        return Ok(())
-                    }
-                }
                 while unsafe { blk_queue_empty_req_helper() == 0 && blk_queue_full_resp_helper() == 0 } {
                     // Assume we magically get the value from sddf
                     let mut request_code: BlkOp = BlkOp::BlkReqRead;
@@ -108,61 +93,76 @@ impl<'a, T: SdmmcHardware> Handler for HandlerImpl<'a, T> {
                             &mut id as *mut u32,
                         );
                     }
+                    // Translate block from sddf_block and sddf_count to real block number and count
+                    // Since the real block size is 512 byte and sddf_block_transfer size is 4KB
+                    // multiply 8 here
+                    // But I do not like this design as why block driver need to know the real transfer size???
+                    block_number = block_number * 8;
+                    count = count * 8;
 
+                    // Print the retrieved values
+                    /*
+                    debug_println!("io_or_offset: 0x{:x}", io_or_offset);// Simple u64
+                    debug_println!("block_number: {}", block_number);    // Simple u32
+                    debug_println!("count: {}", count);                  // Simple u16
+                    debug_println!("id: {}", id);                        // Simple u32
+                    */
                     match request_code {
                         BlkOp::BlkReqRead => {
+                            // If the future is some, block itself
+                            // Since we polling on 
                             // This match in the loop might seems to be ineffient here as the correct way is create future first and polling on that
                             // future. But as the driver would soon change into poll on interrupt instead of polling until finish, so just leave it for now
-                            loop {
-                                match &mut self.future {
-                                    Some(future) => {
-                                        let waker = create_dummy_waker();
-                                        let mut cx = Context::from_waker(&waker);
-                                        // TODO: I can get rid of this loop once I configure out how to enable interrupt from Linux kernel driver
-                                        match future.as_mut().poll(&mut cx) {
-                                            Poll::Ready((result, sdmmc)) => {
-                                                debug_println!("SDMMC_DRIVER: Future completed with result");
-                                                self.future = None; // Reset the future once done
-                                                self.sdmmc = sdmmc;
-                                                if result.is_err() {
-                                                    unsafe {
-                                                        blk_enqueue_resp_helper(BlkStatus::BlkRespSeekError, 0, id);
-                                                    }
-                                                }
-                                                else {
-                                                    unsafe {
-                                                        blk_enqueue_resp_helper(BlkStatus::BlkRespOk, count as u32, id);
-                                                    }
-                                                }
-                                                break;
-                                            }
-                                            Poll::Pending => {
-                                                debug_println!("SDMMC_DRIVER: Future is not ready, polling again...");
-                                            }
-                                        }
-                                        // Send the result back to sddf_blk here
-                                    }
-                                    None => {
-                                        if let Some(sdmmc) = self.sdmmc.take() {
-                                            self.future = Some(Box::pin(sdmmc.read_block(count as u32, block_number as u64, io_or_offset)));
-                                        }
-                                        else {
-                                            panic!("SDMMC_DRIVER: No sdmmc when future is not available which is an undefined state")
-                                        }
-                                    }
-                                }
+                            if let Some(sdmmc) = self.sdmmc.take() {
+                                self.future = Some(Box::pin(sdmmc.read_block(count as u32, block_number as u64, io_or_offset)));
+                            }
+                            else {
+                                panic!("SDMMC_DRIVER: The sdmmc should be here and the future should be empty!!!")
                             }
                         }
                         BlkOp::BlkReqWrite => {
-                            debug_println!("SDMMC_DRIVER: CARD IS READ ONLY FOR NOW!");
+                            if let Some(sdmmc) = self.sdmmc.take() {
+                                self.future = Some(Box::pin(sdmmc.write_block(count as u32, block_number as u64, io_or_offset)));
+                            }
+                            else {
+                                panic!("SDMMC_DRIVER: The sdmmc should be here and the future should be empty!!!")
+                            }
                         },
-                        BlkOp::BlkReqFlush => return Ok(()),
-                        BlkOp::BlkReqBarrier => return Ok(()),
+                        _ => ()
                     }
                     // Notify the virtualizer when there are results available
+                    // TODO: Add retry if the sdcard return an error
+                    if let Some(future) = &mut self.future {
+                        let waker = create_dummy_waker();
+                        let mut cx = Context::from_waker(&waker);
+                        // TODO: I can get rid of this loop once I configure out how to enable interrupt from Linux kernel driver
+                        loop {
+                            match future.as_mut().poll(&mut cx) {
+                                Poll::Ready((result, sdmmc)) => {
+                                    debug_println!("SDMMC_DRIVER: Future completed with result");
+                                    self.future = None; // Reset the future once done
+                                    self.sdmmc = sdmmc;
+                                    if result.is_err() {
+                                        unsafe {
+                                            blk_enqueue_resp_helper(BlkStatus::BlkRespSeekError, 0, id);
+                                        }
+                                    }
+                                    else {
+                                        unsafe {
+                                            blk_enqueue_resp_helper(BlkStatus::BlkRespOk, count as u32, id);
+                                        }
+                                    }
+                                    break;
+                                }
+                                Poll::Pending => {
+                                    debug_println!("SDMMC_DRIVER: Future is not ready, polling again...");
+                                }
+                            }
+                        }
+                    }
+                    debug_println!("SDMMC_DRIVER: Notify BLK_VIRTUALIZER");
                     BLK_VIRTUALIZER.notify();
                 }
-
             }
             _ => {
                 debug_println!("SDMMC_DRIVER: MESSAGE FROM UNKNOWN CHANNEL: {}", channel.index());
@@ -171,165 +171,3 @@ impl<'a, T: SdmmcHardware> Handler for HandlerImpl<'a, T> {
         Ok(())
     }
 }
-
-fn parse_cfg(cfg_register: u32) {
-    // Bits 31:28 - Cfg_ip_txd_adj
-    let ip_txd_adj = (cfg_register >> 28) & 0xF; // 4-bit field
-    debug_println!("Cfg_ip_txd_adj (bits 31:28): {}", ip_txd_adj);
-
-    // Bit 27 - Cfg_err_abort
-    let err_abort = (cfg_register >> 27) & 0x1;
-    debug_println!("Cfg_err_abort (bit 27): {}", err_abort);
-
-    // Bit 26 - Cfg_irq_ds
-    let irq_ds = (cfg_register >> 26) & 0x1;
-    debug_println!("Cfg_irq_ds (bit 26): {}", irq_ds);
-
-    // Bit 25 - Cfg_txd_retry
-    let txd_retry = (cfg_register >> 25) & 0x1;
-    debug_println!("Cfg_txd_retry (bit 25): {}", txd_retry);
-
-    // Bit 24 - Cfg_txd_add_err
-    let txd_add_err = (cfg_register >> 24) & 0x1;
-    debug_println!("Cfg_txd_add_err (bit 24): {}", txd_add_err);
-
-    // Bit 23 - Cfg_auto_clk
-    let auto_clk = (cfg_register >> 23) & 0x1;
-    debug_println!("Cfg_auto_clk (bit 23): {}", auto_clk);
-
-    // Bit 22 - Cfg_stop_clk
-    let stop_clk = (cfg_register >> 22) & 0x1;
-    debug_println!("Cfg_stop_clk (bit 22): {}", stop_clk);
-
-    // Bit 21 - Cfg_cmd_low
-    let cmd_low = (cfg_register >> 21) & 0x1;
-    debug_println!("Cfg_cmd_low (bit 21): {}", cmd_low);
-
-    // Bit 20 - Reserved (skip this)
-
-    // Bit 19 - Cfg_ignore_owner
-    let ignore_owner = (cfg_register >> 19) & 0x1;
-    debug_println!("Cfg_ignore_owner (bit 19): {}", ignore_owner);
-
-    // Bit 18 - Cfg_sdclk_always_on
-    let sdclk_always_on = (cfg_register >> 18) & 0x1;
-    debug_println!("Cfg_sdclk_always_on (bit 18): {}", sdclk_always_on);
-
-    // Bit 17 - Cfg_blk_gap_ip
-    let blk_gap_ip = (cfg_register >> 17) & 0x1;
-    debug_println!("Cfg_blk_gap_ip (bit 17): {}", blk_gap_ip);
-
-    // Bit 16 - Cfg_out_fall
-    let out_fall = (cfg_register >> 16) & 0x1;
-    debug_println!("Cfg_out_fall (bit 16): {}", out_fall);
-
-    // Bits 15:12 - Cfg_rc_cc
-    let rc_cc = (cfg_register >> 12) & 0xF; // 4-bit field
-    debug_println!("Cfg_rc_cc (bits 15:12): {}", rc_cc);
-
-    // Bits 11:8 - Cfg_resp_timeout
-    let resp_timeout = (cfg_register >> 8) & 0xF; // 4-bit field
-    debug_println!("Cfg_resp_timeout (bits 11:8): {}", resp_timeout);
-
-    // Bits 7:4 - Cfg_bl_len
-    let bl_len = (cfg_register >> 4) & 0xF; // 4-bit field
-    debug_println!("Cfg_bl_len (bits 7:4): {}", bl_len);
-
-    // Bit 3 - Cfg_dc_ugt
-    let dc_ugt = (cfg_register >> 3) & 0x1;
-    debug_println!("Cfg_dc_ugt (bit 3): {}", dc_ugt);
-
-    // Bit 2 - Cfg_ddr
-    let ddr = (cfg_register >> 2) & 0x1;
-    debug_println!("Cfg_ddr (bit 2): {}", ddr);
-
-    // Bits 1:0 - Cfg_bus_width
-    let bus_width = cfg_register & 0x3; // 2-bit field
-    debug_println!("Cfg_bus_width (bits 1:0): {}", bus_width);
-}
-
-fn parse_clock(clock_register: u32) {
-        // Print the raw clock register value
-        debug_println!("Clock register value: {:#034b} (binary), {:#X} (hex)", clock_register, clock_register);
-        
-        // Extracting individual fields based on the documentation:
-    
-        // Bit 30 - Cfg_irq_sdio_sleep_ds
-        let irq_sdio_sleep_ds = (clock_register >> 30) & 0x1;
-        debug_println!("Cfg_irq_sdio_sleep_ds (bit 30): {}", irq_sdio_sleep_ds);
-    
-        // Bit 29 - Cfg_irq_sdio_sleep
-        let irq_sdio_sleep = (clock_register >> 29) & 0x1;
-        debug_println!("Cfg_irq_sdio_sleep (bit 29): {}", irq_sdio_sleep);
-    
-        // Bit 28 - Cfg_always_on
-        let always_on = (clock_register >> 28) & 0x1;
-        debug_println!("Cfg_always_on (bit 28): {}", always_on);
-    
-        // Bits 27:22 - Cfg_rx_delay
-        let rx_delay = (clock_register >> 22) & 0x3F; // 6-bit field
-        debug_println!("Cfg_rx_delay (bits 27:22): {}", rx_delay);
-    
-        // Bits 21:16 - Cfg_tx_delay
-        let tx_delay = (clock_register >> 16) & 0x3F; // 6-bit field
-        debug_println!("Cfg_tx_delay (bits 21:16): {}", tx_delay);
-    
-        // Bit 15:14 - Cfg_sram_pd
-        let sram_pd = (clock_register >> 14) & 0x3; // 2-bit field
-        debug_println!("Cfg_sram_pd (bits 15:14): {}", sram_pd);
-    
-        // Bit 13:12 - Cfg_rx_phase
-        let rx_phase = (clock_register >> 12) & 0x3; // 2-bit field
-        debug_println!("Cfg_rx_phase (bits 13:12): {}", rx_phase);
-    
-        // Bit 11:10 - Cfg_tx_phase
-        let tx_phase = (clock_register >> 10) & 0x3; // 2-bit field
-        debug_println!("Cfg_tx_phase (bits 11:10): {}", tx_phase);
-    
-        // Bit 9:8 - Cfg_co_phase
-        let co_phase = (clock_register >> 8) & 0x3; // 2-bit field
-        debug_println!("Cfg_co_phase (bits 9:8): {}", co_phase);
-    
-        // Bit 7:6 - Cfg_src
-        let cfg_src = (clock_register >> 6) & 0x3; // 2-bit field
-        debug_println!("Cfg_src (bits 7:6): {}", cfg_src);
-    
-        // Bit 5:0 - Cfg_div
-        let cfg_div = clock_register & 0x3F; // 6-bit field
-        debug_println!("Cfg_div (bits 5:0): {}", cfg_div);
-}
-
-/*
-Great! Now I have another question, my sdmmc driver is almost working but there is one thing that I must do since the rest of the system are in C, assuming there is a structure somehow in the memory and it is the driver's responsibility to populate that structure. What is more, to cooperate with the rest of the system, I need to FFI to some functions. What should I do?
-Structure to populate:
-/* Size of a single block to be transferred */
-#define BLK_TRANSFER_SIZE 4096
-/* Device serial number max string length */
-#define BLK_MAX_SERIAL_NUMBER 63
-
-typedef struct blk_storage_info {
-    char serial_number[BLK_MAX_SERIAL_NUMBER + 1];
-    /* device does not accept write requests */
-    bool read_only;
-    /* whether this configuration is populated yet */
-    bool ready;
-    /* size of a sector, in bytes */
-    uint16_t sector_size;
-    /* optimal block size, specified in BLK_TRANSFER_SIZE sized units */
-    uint16_t block_size;
-    uint16_t queue_depth;
-    /* geometry to guide FS layout */
-    uint16_t cylinders, heads, blocks;
-    /* total capacity of the device, specified in BLK_TRANSFER_SIZE sized units. */
-    uint64_t capacity;
-} blk_storage_info_t;
-
-Functions to FFI to:
-static inline void blk_queue_init(blk_queue_handle_t *h,
-                                  blk_req_queue_t *request,
-                                  blk_resp_queue_t *response,
-                                  uint32_t capacity);
-static inline int blk_queue_length_req(blk_queue_handle_t *h);
-static inline int blk_queue_length_resp(blk_queue_handle_t *h);
- 
-*/
